a834fb8898670cd1c7dcf4146fe9433c8aa13aff
CHRIS SKYR <christaraskyr@gmail.com>
2022-10-19 20:36:34 +0300

    account page population

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -13,14 +13,27 @@ const Account = () => {
     }
   };
   return (
-    <div className="account-container">
-      <h1>Account</h1>
-      <div>
-        <p>Welcome, {user?.displayName}</p>
+    <div className="main-container">
+      <ul className="side-bar">
+        <li className="settingsP">
+          <h1>Settings</h1>
+        </li>
+        <li className="accountBtn">
+          <button>Account</button>
+        </li>
+        <li>
+          <button>Notifications</button>
+        </li>
+      </ul>
+      <div className="account-container">
+        <h1>Account</h1>
+        <div>
+          <p>Welcome, {user?.displayName}</p>
+        </div>
+        <button onClick={handleSignOut} className="logoutBtn">
+          logOut
+        </button>
       </div>
-      <button onClick={handleSignOut} className="logoutBtn">
-        logOut
-      </button>
     </div>
   );
 };
